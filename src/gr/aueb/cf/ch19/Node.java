4e4b15b3ce45dd45f1e2fb43b25b32a694de6cae
Nektarios Tsagkaris <nektarios.tsagkaris@gmail.com>
2025-02-03 19:02:04 +0200

    Generics

---
+++ src/gr/aueb/cf/ch19/Node.java
@@ -0,0 +1,15 @@
+package gr.aueb.cf.ch19;
+
+//kombos
+
+public class Node {
+    private int item;
+
+    public int getItem() {
+        return item;
+    }
+
+    public void setItem(int item) {
+        this.item = item;
+    }
+}
